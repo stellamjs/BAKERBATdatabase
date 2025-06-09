dd77142d8e25d85f651992210cdcb8f10c5721ce
stellamjs <stella.mjs@gmail.com>
2025-06-09 09:49:25 -0400

    initialize BAKERBATdatabase package

---
+++ R/connection_function.R
@@ -0,0 +1,33 @@
+## bakerbat_database connecting function
+#' This function simplifies connecting with the bakerbat_database. It calls the username
+#' and password saved as a .txt file with username as the first line and password as the second line.
+#' Make sure there is not superfluous white space in this text file.
+#' @param username.password.file.path The path of the text file containing the username and password to the database account
+#' @param suppress.bat Set to TRUE 
+#'
+#' @return The database connection object 
+#' @export
+#'
+
+
+bakerbat_database_connect <- function(username.password.file.path, suppress.bat = T) {
+  credentials <- suppressWarnings(read.delim(username.password.file.path, header = F))
+
+  my_username <- as.character(credentials[1, 1])
+  my_password <- as.character(credentials[2, 1])
+
+  mydb <- "temp"
+  mydb = RMySQL::dbConnect(RMySQL::MySQL(),
+                           user = my_username,
+                           password = my_password,
+                           dbname = 'bakerbat_database',
+                           host = "preempt-database.cy5swjbpwcs6.us-west-2.rds.amazonaws.com")
+
+  if (!is.character(mydb) & !(suppress.bat)) {
+    cat("Success! Thank you for connecting with the bakerbat_database"
+    )
+  }
+  return(mydb)
+}
+
+
