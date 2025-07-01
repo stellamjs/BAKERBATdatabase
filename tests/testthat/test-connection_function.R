7a4e0727db0167031ff2cc23aa805d7aeb18f4d7
stellamjs <stella.mjs@gmail.com>
2025-07-01 11:15:25 -0400

    add test to connect function

--- tests/testthat/test-connection_function.R
+++ tests/testthat/test-connection_function.R
@@ -1,3 +1,3 @@
 test_that("connect to the bakerbat_database", {
-  expect_equal(con<-bakerbat_database_connect('/home/stella/Desktop/mycredentials.txt', 'con'))
+  expect_equal(con<-bakerbat_database_connect('/home/stella/Desktop/mycredentials.txt'), con)
 })
